0a4528b1c5d61ed83d7a44cf8254650ce5b3a3e0
Juliet Li <li_juliet@hotmail.com>
2021-07-30 19:45:40 +0000

    promised version

---
+++ index2.js
@@ -0,0 +1,14 @@
+const { nextISSTimesForMyLocation } = require('./iss_promised')
+
+// fetchMyIP()
+//   .then(fetchCoordsByIP)
+//   .then(fetchISSFlyOverTimes)
+//   .then(body => console.log(body));
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    console.log(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
